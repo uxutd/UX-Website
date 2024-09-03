138510f2113cc708cae5d65032cff36715d21021
Kshitij Kulshrestha <kshitijkuls6@gmail.com>
2024-09-03 15:04:43 -0500

    will fix animated-shiny-test.tsx

--- components/magicui/animated-shiny-text.tsx
+++ components/magicui/animated-shiny-text.tsx
@@ -1,5 +1,4 @@
 import { CSSProperties, FC, ReactNode } from "react";
-
 import { cn } from "@/lib/utils";
 
 interface AnimatedShinyTextProps {
@@ -17,14 +16,11 @@ const AnimatedShinyText: FC<AnimatedShinyTextProps> = ({
     <p
       className={cn(
         "mx-auto max-w-md text-neutral-600/70 dark:text-neutral-400/70",
-
-        // Shimmer effect
-        "animate-shimmer bg-clip-text bg-no-repeat [background-position:0_0] [background-size:var(--shimmer-width)_100%] [transition:background-position_1s_cubic-bezier(.6,.6,0,1)_infinite]",
-
-        // Shimmer gradient
-        "bg-gradient-to-r from-transparent via-black/80 via-50% to-transparent  dark:via-white/80",
-
-        className,
+        "animate-shimmer bg-clip-text bg-no-repeat",
+        // Shimmer effect and gradient
+        "[background-position:0_0] [background-size:var(--shimmer-width)_100%] transition-[background-position_1s_cubic-bezier(.6,.6,0,1)_infinite]",
+        "bg-gradient-to-r from-transparent via-black/80 via-50% to-transparent dark:via-white/80",
+        className
       )}
       style={
         {
