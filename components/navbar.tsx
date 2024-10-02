5e247ecbfb5a1de26252a786a58ecfb65185f9e2
Kshitij Kulshrestha <kshitijkuls6@gmail.com>
2024-10-02 04:07:44 -0500

    will change the only link visible to discord on mobile

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -91,8 +91,8 @@ export const Navbar = () => {
       </NavbarContent>
 
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal aria-label="Github" href={siteConfig.links.github}>
-          <GithubIcon className="text-default-500" />
+        <Link isExternal aria-label="Discord" href={siteConfig.links.discord}>
+          <DiscordIcon className="text-default-500" />
         </Link>
         <ThemeSwitch />
         <NavbarMenuToggle />
