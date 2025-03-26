42236402c072c2992eb090509be614228a026917
Kshitij Kulshrestha <kshitijkuls6@gmail.com>
2025-03-25 20:46:09 -0500

    fixed issues with AnimatePresence dependency

--- components/ui/animated-testimonials.tsx
+++ components/ui/animated-testimonials.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { IconArrowLeft, IconArrowRight } from "@tabler/icons-react";
-import { motion, AnimatePresence } from "motion/react";
+import { motion, AnimatePresence } from "framer-motion";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
