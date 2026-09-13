81326cb45d1d6f03c72b48bedd1607597f5a44ff
AO5002 <154623452+AO-5002@users.noreply.github.com>
2026-09-12 23:02:35 -0500

    Upgraded to Next 16.3.5
    - Bumped NextUI packages (2.0.36 -> 2.2.8) to fix React crash.
    - Upgraded npm packages (npm audit -> 0 vulnerabilities).
    - No UI changes.
    - Remaining terminal warnings are deprecated image props, non-breaking.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Enabling React Strict Mode (optional, but good for development)
-    reactStrictMode: true,
+  // Enabling React Strict Mode (optional, but good for development)
+  reactStrictMode: true,
 
-    // Enabling SWC Minification
-    swcMinify: true,
+  // Enabling SWC Minification
+  //   swcMinify: true,
 };
 
 module.exports = nextConfig;
